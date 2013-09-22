84d329aba0df50434e72a0cbfaa99d85befef318
Daniel Platt <github@ofdan.co.uk>
2013-09-22 15:28:48 +0100

    add basic test
    partially completed mocks

--- Tests/User/FOSUserTest.php
+++ Tests/User/FOSUserTest.php
@@ -10,7 +10,8 @@ class FOSUserTest extends WebTestCase
 
     public function setUp()
     {
-        $this->_object = new \Hackzilla\Bundle\FOSUserBridgeBundle\User\FOSUser();
+        $container = $this->getMockContainer();
+        $this->_object = new \Hackzilla\Bundle\FOSUserBridgeBundle\User\FOSUser($container);
     }
 
     public function tearDown()
@@ -21,6 +22,43 @@ class FOSUserTest extends WebTestCase
     public function testObjectCreated()
     {
         $this->assertTrue(\is_object($this->_object));
+    }
+
+    private function getMockContainer()
+    {
+        $container = $this->getMock('Symfony\Component\DependencyInjection\Container');
+        $container->expects($this->exactly(2))
+                    ->method('get')
+                    ->will($this->returnCallback(array($this, 'getServiceMock')));
+
+        return $container;
+    }
+
+    public function getServiceMock($serviceName)
+    {
+        if ($serviceName == 'security.context') {
+            $service = $this->getMockSecurity();
+        } else if ($serviceName == 'user.manager') {
+            $service = $this->getMockUserManager();
+        } else {
+            $service = false;
+        }
+
+        return $service;
+    }
+    
+    private function getMockSecurity()
+    {
+        $security = $this->getMock('stdClass');
+
+        return $security;
+    }
+
+    private function getMockUserManager()
+    {
+        $userManager = $this->getMock('stdClass');
+
+        return $userManager;
     }
 }
 
