b7a354de65ab7984f0671c6c88b0835a58f0037e
Daniel Platt <github@ofdan.co.uk>
2015-01-06 18:44:46 +0000

    type hint container

--- User/FOSUser.php
+++ User/FOSUser.php
@@ -3,11 +3,12 @@
 namespace Hackzilla\Bundle\FOSUserBridgeBundle\User;
 
 class FOSUser implements \Hackzilla\Interfaces\User\UserInterface
+use Symfony\Component\DependencyInjection\ContainerInterface;
 {
     private $securityContext;
     private $userManager;
 
-    function __construct($container)
+    function __construct(ContainerInterface $container)
     {
         $this->securityContext = $container->get('security.context');
         $this->userManager = $container->get('fos_user.user_manager');
